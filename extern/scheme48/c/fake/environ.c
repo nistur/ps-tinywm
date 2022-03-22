8627106142d78da6c05a89c18f56964c2e38185e
Philipp Geyer <philipp@geyer.co.uk>
2022-03-22 10:27:16 +0000

    switching to submodule for scheme48 so we can work on adding features separately

--- extern/scheme48/c/fake/environ.c
+++
@@ -1,9 +0,0 @@
-/*
- * Part of Scheme 48 1.9.  See file COPYING for notices and license.
- *
- * Authors: Richard Kelsey, Jonathan Rees
- */
-
-#include <stdlib.h>
-
-char	**s48_bogus_environ = NULL;
--- extern/scheme48/scheme/env/version-info.scm
+++
@@ -1,3 +0,0 @@
-; Part of Scheme 48 1.9.  See file COPYING for notices and license.
-
-(define version-info "1.9.2")
